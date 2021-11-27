db599ee35e33d637410028a28572043f251ba0ea
danial23 <5867710+danial23@users.noreply.github.com>
2021-11-27 17:46:35 -0500

    Update mediawiki_extractor.py
    Added a get_all_revisions() function which pulls revision data from API repeatedly until the query is finished.

--- mediawiki_extractor.py
+++ mediawiki_extractor.py
@@ -4,28 +4,69 @@
 # Sample script using wikimedia API to extract data into CSV files
 
 import requests
+import json
+import time
 
 S = requests.Session()
+S.headers.update({"User-Agent" : "script (danialmtmdmhr23@gmail.com)", "Accept-encoding" : "gzip"})
+API = "https://en.wikipedia.org/w/api.php"
 
-URL = "https://en.wikipedia.org/w/api.php"
 
-PARAMS = {
-    "action": "query",
-    "prop": "revisions",
-    "titles": "Let Us Continue",
-    "rvlimit": "100",
-    "rvprop": "timestamp|user|comment|context|tags",
-    "rvdir": "newer",
-    "rvslots": "main",
-    "formatversion": "2",
-    "format": "json"
-}
+def dump_data(data):
+    pass
 
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
 
-PAGES = DATA["query"]["pages"]
+def get_all_revisions(title):
+    start_time = time.time() #####
 
-for page in PAGES:
-    for rev in page["revisions"]:
-        print(rev)
+    PARAMS = {
+        "action": "query",
+        "prop": "revisions",
+        "titles": title,
+        "rvprop": "flags|timestamp|userid|size|comment|tags",
+        "rvslots": "main",
+        "rvlimit": "max",
+        "formatversion": "2",
+        "format": "json",
+        "maxlag" : "2",
+    }
+
+    R = S.get(url=API, params=PARAMS)
+    DATA = R.json()
+
+    if "error" in DATA or "warning" in DATA:
+        print(json.dumps(DATA, indent=4))
+        return None
+    
+    PAGE = DATA["query"]["pages"][0]
+    revisions = PAGE["revisions"]
+
+    while "continue" in DATA:
+
+        PARAMS.update({"rvcontinue" : DATA["continue"]["rvcontinue"]})
+        
+        R = S.get(url=API, params=PARAMS)
+        DATA = R.json()
+
+        if "error" in DATA or "warning" in DATA:
+            print(json.dumps(DATA, indent=4))
+            return None
+        
+        PAGE = DATA["query"]["pages"][0]
+        revisions += PAGE["revisions"]
+    
+    end_time = time.time() #####
+
+    print(end_time - start_time) ####
+
+    return revisions
+
+
+def main():
+    revisions = get_all_revisions("SQLite")
+    print(revisions)
+    print(len(revisions))
+
+
+if __name__ == "__main__":
+    main()
