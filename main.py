d164d8d29dd7b60ca98d9ebdc1bdcee992ec0d92
Molly Jacobsen <33874150+MollyJacobsen@users.noreply.github.com>
2021-11-29 17:20:45 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import wikipedia_histories
 import pandas as pd
+import os
 
+# df = pd.DataFrame()
 
+## assuming our data is stored in csv files is some dir "dataset"
+# for file in os.listdir("./dataset/"):
+#    curdf = pd.read_csv(file)
+#    df.append(curdf)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    Let_Us_Continue = wikipedia_histories.get_history('Let Us Continue')
-    print(Let_Us_Continue)
-    print(Let_Us_Continue[240].content)
-    print(len(Let_Us_Continue))
-    df = pd.DataFrame(Let_Us_Continue)
-    df.to_csv('Let_Us_Continue.csv')
-
-    with open('Let_Us_Continue.txt', 'w') as f:
-        for i in Let_Us_Continue:
-            f.write("%s\n" % i.content)
