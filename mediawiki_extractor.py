e53b51aecb1cb90d72f2e254cefc7c1e499480d9
Tig <tionni>
2021-11-29 00:58:46 -0500

    added methods to count number of reverted/undo tags and to dump rivisions to txt

--- mediawiki_extractor.py
+++ mediawiki_extractor.py
@@ -8,16 +8,16 @@ import json
 import time
 
 S = requests.Session()
-S.headers.update({"User-Agent" : "script (danialmtmdmhr23@gmail.com)", "Accept-encoding" : "gzip"})
+S.headers.update({"User-Agent": "script (danialmtmdmhr23@gmail.com)", "Accept-encoding": "gzip"})
 API = "https://en.wikipedia.org/w/api.php"
 
 
-def get_all_revisions(title : str) -> list:
+def get_all_revisions(title: str) -> list:
     """
     Repeatedly sends requests to API to get ALL revision data
     """
-    
-    start_time = time.time() # for benchmarking
+
+    start_time = time.time()  # for benchmarking
 
     # request parameters
     PARAMS = {
@@ -26,10 +26,10 @@ def get_all_revisions(title : str) -> list:
         "titles": title,
         "rvprop": "flags|timestamp|userid|size|comment|tags|content",
         "rvslots": "main",
-        "rvlimit": "max", # 50 when contents requested, 500-5000 otherwise
+        "rvlimit": "max",  # 50 when contents requested, 500-5000 otherwise
         "formatversion": "2",
         "format": "json",
-        "maxlag" : "2", # lower is nicer, should be lower than 5
+        "maxlag": "2",  # lower is nicer, should be lower than 5
     }
 
     # send request
@@ -42,7 +42,7 @@ def get_all_revisions(title : str) -> list:
     if "error" in DATA or "warning" in DATA:
         print(json.dumps(DATA, indent=4))
         return None
-    
+
     # we only have a single page in query
     PAGE = DATA["query"]["pages"][0]
     revisions = PAGE["revisions"]
@@ -51,30 +51,48 @@ def get_all_revisions(title : str) -> list:
     while "continue" in DATA:
 
         # modify parameters to include last query's rvcontinue
-        PARAMS.update({"rvcontinue" : DATA["continue"]["rvcontinue"]})
-        
+        PARAMS.update({"rvcontinue": DATA["continue"]["rvcontinue"]})
+
         R = S.get(url=API, params=PARAMS)
         DATA = R.json()
 
         if "error" in DATA or "warning" in DATA:
             print(json.dumps(DATA, indent=4))
             return None
-        
+
         PAGE = DATA["query"]["pages"][0]
 
         # append to the revision list
         revisions += PAGE["revisions"]
-    
-    end_time = time.time() # for benchmarking
 
-    print("Total time for page \"" + title + "\":", end_time - start_time) # for benchmarking
-    print("Total revisions:", len(revisions)) # for benchmarking
+    end_time = time.time()  # for benchmarking
+
+    print("Total time for page \"" + title + "\":", end_time - start_time)  # for benchmarking
+    print("Total revisions:", len(revisions))  # for benchmarking
 
     return revisions
 
 
 def main():
-    revisions = get_all_revisions("SQLite")
+    revisions = get_all_revisions("Jaguar")
+    count = count_reverts(revisions)
+    print(count)
+
+
+def save_revisions(revisions, filename):
+    with open(filename, 'w') as f:
+        for i in revisions:
+            f.write("%s\n" % i)
+
+
+def count_reverts(revisions):
+    count = 0
+    for i in revisions:
+        # print(i["tags"])
+        for q in i["tags"]:
+            if q == 'mw-undo' or q == 'mw-reverted' or q == 'Reverted' or q == 'mw-manual-revert':
+                count = count + 1
+    return count
 
 
 if __name__ == "__main__":
