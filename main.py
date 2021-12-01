6b3bbb71df346851f1bf285b3ca64d0ff1f987b3
Molly Jacobsen <33874150+MollyJacobsen@users.noreply.github.com>
2021-12-01 17:28:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,36 @@ import preprocessor as pp
 import mediawiki_extractor as mwe
 
 revisions = []
+directory = os.fsencode("./dataset/")
 
 ## assuming our data is stored in json files in some dir "dataset"
-for file in os.listdir("./dataset/"):
-    cur_f = open(file)
+for file in os.listdir(directory):
+    cur_f = open(os.path.join(directory, file))
     d = json.load(cur_f)
-    for r in d:
-        revisions.append(r)
+    try:
+        for r in d["revisions"]:
+            revisions.append(r)
+    except:
+        continue
 
 
 tags = []
 content = []
 
 for i, r in enumerate(revisions):
-    tags.append(r["tags"])
-    content.append(r["slots"]["main"]["content"])
+    try:
+        tags.append(r["tags"])
+        content.append(r["slots"]["main"]["content"])
+    except:
+        continue
 
-targets = pp.tagsToTargets(tags)
+targets = pp.tagsToTargets(revisions)
 data = pp.contentToNgramVectors(content, 1, 2)
+
+if len(targets) > len(data):
+    data = data[:len(targets)]
+elif len(data) > len(targets):
+    targets = targets[:len(data)]
 
 d_train, d_test, t_train, t_test = train_test_split(data, targets, test_size=0.15)
 
