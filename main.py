81067e77f980c1edf956141ef1d93de96cf5ddb7
Molly Jacobsen <33874150+MollyJacobsen@users.noreply.github.com>
2021-11-30 23:45:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import pandas as pd
 import os
+import json
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import preprocessor as pp
+import mediawiki_extractor as mwe
 
-df = pd.DataFrame()
+revisions = []
 
-## assuming our data is stored in csv files is some dir "dataset"
+## assuming our data is stored in json files in some dir "dataset"
 for file in os.listdir("./dataset/"):
-    curdf = pd.read_csv(file)
-    df.append(curdf)
+    cur_f = open(file)
+    d = json.load(cur_f)
+    for r in d:
+        revisions.append(r)
 
-targets = pp.tagsToTargets(df['tags'])
-diffData = pp.contentToDiff(df['content'])
+
+tags = []
+content = []
+
+for i, r in enumerate(revisions):
+    tags.append(r["tags"])
+    content.append(r["slots"]["main"]["content"])
+
+targets = pp.tagsToTargets(tags)
+targets.pop()   #because we're only looking at the differences
+diffData = pp.contentToDiff(content)
 data = pp.contentToNgramVectors(diffData, 1, 2)
 
 d_train, d_test, t_train, t_test = train_test_split(data, targets, test_size=0.15)
 
 clf = LogisticRegression(random_state=0, max_iter=1000)
 clf.fit(d_train, t_train)
+
+score = clf.score(d_test, t_test)
+print(score)
