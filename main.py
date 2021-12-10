5d4b80f0dae7e99e5ddc5924914ca6197a5c253e
Molly Jacobsen <33874150+MollyJacobsen@users.noreply.github.com>
2021-12-09 20:23:02 -0500

    Update main.py
    now reads vectorized data from vectorized_data.npz file

--- main.py
+++ main.py
@@ -4,49 +4,79 @@ import json
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import preprocessor as pp
-import mediawiki_extractor as mwe
+from scipy import sparse
 
-revisions = []
 
-directory = os.fsencode("./dataset/")
+def performance(mname, revs, predictions, goals):
+    print(mname + " performance: ")
+    tp, fp, tn, fn = 0, 0, 0, 0
+    for g, p in zip(goals, predictions):
+        if p != g:
+            if p == 1:
+                fp += 1
+                print('Non-reverted revision with comment "' + revs[goals.index(g)]["comment"] + '" was predicted to be reverted.')
+            else:
+                fn += 1
+                print('Reverted revision with comment "' + revs[goals.index(g)]["comment"] + '" was missed.')
+        else:
+            if p == 1:
+                tp += 1
+            else:
+                tn += 1
+    print("Total false positives: " + str(fp))
+    print("Total true positives: " + str(tp))
+    print("Total false negatives: " + str(fn))
+    print("Total true negatives: " + str(tn))
+    print("Total mistakes: " + str(fp + fn))
+    accuracy = (tp+tn)/(fp+fn+tp+tn)
+    print("Accuracy: " + str(accuracy * 100) + "%")
+    precision = tp/(fp+tp)
+    print("Precision: " + str(precision * 100) + "%")
+    recall = tp/(fn+tp)
+    print("Recall: " + str(recall * 100) + "%")
+    specificity = tn/(fp+tn)
+    print("Specificity: " + str(specificity * 100) + "%")
+    print("F1: " + str(((2*precision*recall)/(precision+recall)) * 100) + "%")
 
-## assuming our data is stored in json files in some dir "dataset"
-for file in os.listdir(directory):
-    cur_f = open(os.path.join(directory, file))
-    d = json.load(cur_f)
+if __name__ == "__main__":
+    revisions = []
+
+    with open('titles.json', "r") as f:
+        titles = json.load(f)
+
+    os.chdir('dataset')
+
     try:
-        for r in d["revisions"]:
-            revisions.append(r)
-    except:
-        continue
+        with open('titles_fetched.json', 'r') as f:
+            titles_fetched = json.load(f)
+    except FileNotFoundError:
+        titles_fetched = {}
 
+    ## assuming our data is stored in json files numbered as they are in tites_fetched.json
+    for index, title in enumerate(titles):
 
-tags = []
-content = []
+        if title not in titles_fetched:
+            continue
 
-for r in revisions:
-    try:
-        tags.append(r["tags"])
-        content.append(r["slots"]["main"]["content"])
-    except:
-        continue
+        try:
+            cur_f = open(str(index) + ".json")
+            d = json.load(cur_f)
+            for r in d["revisions"]:
+                revisions.append(r)
+        except:
+            continue
 
-targets = pp.tagsToTargets(revisions)
+    data = sparse.load_npz("vectorized_data.npz")
 
-if len(targets) > len(content):
-    targets = targets[:len(content)]
-elif len(content) > len(targets):
-    content = content[:len(targets)]
+    targets = pp.tagsToTargets(revisions)
+    #content is always 1 element shorter than tags
+    targets.pop()   
 
-data = pp.contentToNgramVectors(content, 1, 2)
+    d_train, d_test, t_train, t_test = train_test_split(data, targets, test_size=0.15)
 
-d_train, d_test, t_train, t_test = train_test_split(data, targets, test_size=0.15)
+    clf = LogisticRegression(random_state=0, max_iter=1000)
+    clf.fit(d_train, t_train)
 
-clf = LogisticRegression(random_state=0, max_iter=1000)
-clf.fit(d_train, t_train)
+    results = clf.predict(d_test)
 
-
-results = clf.predict(d_test)
-
-
-pp.performance("Logistic Regression Model", revisions, results, t_test)
+    performance("Logistic Regression Model", revisions, results, t_test)
