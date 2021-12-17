90c28b0cf475cf4386db2d904a880f49db5b35fd
Molly Jacobsen <33874150+MollyJacobsen@users.noreply.github.com>
2021-12-17 15:42:54 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import pandas as pd
 import os
+import csv
 import json
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
-import preprocessor as pp
 from scipy import sparse
 
 
-def performance(mname, revs, predictions, goals):
+def performance(mname, predictions, goals):
     print(mname + " performance: ")
     tp, fp, tn, fn = 0, 0, 0, 0
     for g, p in zip(goals, predictions):
         if p != g:
-            if p == 1:
+            if p == '1':
                 fp += 1
-                print('Non-reverted revision with comment "' + revs[goals.index(g)]["comment"] + '" was predicted to be reverted.')
             else:
                 fn += 1
-                print('Reverted revision with comment "' + revs[goals.index(g)]["comment"] + '" was missed.')
         else:
-            if p == 1:
+            if p == '1':
                 tp += 1
             else:
                 tn += 1
@@ -28,55 +28,51 @@ def performance(mname, revs, predictions, goals):
     print("Total false negatives: " + str(fn))
     print("Total true negatives: " + str(tn))
     print("Total mistakes: " + str(fp + fn))
-    accuracy = (tp+tn)/(fp+fn+tp+tn)
-    print("Accuracy: " + str(accuracy * 100) + "%")
-    precision = tp/(fp+tp)
-    print("Precision: " + str(precision * 100) + "%")
-    recall = tp/(fn+tp)
-    print("Recall: " + str(recall * 100) + "%")
-    specificity = tn/(fp+tn)
-    print("Specificity: " + str(specificity * 100) + "%")
-    print("F1: " + str(((2*precision*recall)/(precision+recall)) * 100) + "%")
+    try:
+        accuracy = (tp+tn)/(fp+fn+tp+tn)
+        print("Accuracy: " + str(accuracy * 100) + "%")
+        precision = tp/(fp+tp)
+        print("Precision: " + str(precision * 100) + "%")
+        recall = tp/(fn+tp)
+        print("Recall: " + str(recall * 100) + "%")
+        specificity = tn/(fp+tn)
+        print("Specificity: " + str(specificity * 100) + "%")
+        print("F1: " + str(((2*precision*recall)/(precision+recall)) * 100) + "%")
+    except ZeroDivisionError:
+        print("Division by zero")
+
 
 if __name__ == "__main__":
-    revisions = []
 
-    with open('titles.json', "r") as f:
-        titles = json.load(f)
+    os.chdir("dataset")
 
-    os.chdir('dataset')
+    nb = MultinomialNB()
+    sgd = SGDClassifier()
 
-    try:
-        with open('titles_fetched.json', 'r') as f:
-            titles_fetched = json.load(f)
-    except FileNotFoundError:
-        titles_fetched = {}
+    with open("targets.csv", "r") as f:
+        targetreader = list(csv.reader(f))
 
-    ## assuming our data is stored in json files numbered as they are in tites_fetched.json
-    for index, title in enumerate(titles):
+    test_data = []
+    test_targets = []
 
-        if title not in titles_fetched:
-            continue
+    for i in range(6):
+        file_name = "vectorized_data-" + str(i) + ".npz"
+        if os.path.isfile(file_name):
+            data = sparse.load_npz(file_name)
+            targets = targetreader[i]
 
-        try:
-            cur_f = open(str(index) + ".json")
-            d = json.load(cur_f)
-            for r in d["revisions"]:
-                revisions.append(r)
-        except:
-            continue
+            train_d, test_d, train_t, test_t = train_test_split(data, targets, test_size=0.10)
+            test_data.append(test_d)
+            test_targets += test_t
+            nb.partial_fit(train_d, train_t, classes=['0', '1'])
+            sgd.partial_fit(train_d, train_t, classes=['0', '1'])
 
-    data = sparse.load_npz("vectorized_data.npz")
+    nb_results = []
+    sgd_results = []
+    for data in test_data:
+        nb_results += list(nb.predict(data))
+        sgd_results += list(sgd.predict(data))
 
-    targets = pp.tagsToTargets(revisions)
-    #content is always 1 element shorter than tags
-    targets.pop()   
-
-    d_train, d_test, t_train, t_test = train_test_split(data, targets, test_size=0.15)
-
-    clf = LogisticRegression(random_state=0, max_iter=1000)
-    clf.fit(d_train, t_train)
-
-    results = clf.predict(d_test)
-
-    performance("Logistic Regression Model", revisions, results, t_test)
+    performance("Multinomial Naive Bayes Model", nb_results, test_targets)
+    print('\n')
+    performance("SGD Classifier", sgd_results, test_targets)
